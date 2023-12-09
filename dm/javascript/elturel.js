9a26d406e8b4736fc1d167670f04d3c849a6dcc9
NullDefault <davisha999@gmail.com>
2023-12-09 16:24:43 -0500

    lines

--- dm/javascript/elturel.js
+++ dm/javascript/elturel.js
@@ -24,6 +24,36 @@ let mapComponent = {
   components: {
     "location-marker": locationComponent,
   },
+  methods: {
+    calculateLineStyle(location1, location2) {
+        console.log(location1.numId)
+        console.log(location2.numId)
+      if (!location2) {
+        // If no matching location is found, return an empty style object or handle the error as needed
+        return {};
+      }
+
+      const x1 = location1.x;
+      const y1 = location1.y;
+      const x2 = location2.x;
+      const y2 = location2.y;
+      const length = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+      const angle = (Math.atan2(y2 - y1, x2 - x1) * 180) / Math.PI;
+
+      return {
+        position: "absolute",
+        top: y1 + "px",
+        left: x1 + "px",
+        width: length + "px",
+        transform: `rotate(${angle}deg)`,
+        transformOrigin: "0 0",
+        height: "4px",
+        border: "1px solid white",
+        backgroundColor: "black",
+        zIndex: 20
+      };
+    },
+  },
 };
 
 var app = new Vue({
